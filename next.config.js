c32fed2096753c650f25016c8eef4fc89fc09621
Shivam Sharma <shivam.sharma.ug21@nsut.ac.in>
2024-05-24 05:05:20 +0530

    new Deploy

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    output: "export",
+    appDir: false,
+    basePath: "/MetaVerse_Website/",
+  },
+};
+
+module.exports = nextConfig;
